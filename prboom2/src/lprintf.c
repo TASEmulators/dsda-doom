85f0d969ce85a0badfc60858ccb429589f3943c2
feos <feykomylce@gmail.com>
2025-11-16 12:39:11 +0300

    fix?

--- prboom2/src/lprintf.c
+++ prboom2/src/lprintf.c
@@ -56,7 +56,7 @@
 
 #include "dsda/args.h"
 
-extern void biz_error_callback(char *s);
+extern void biz_error_callback(const char *s);
 
 dboolean enableOutput;
 static dboolean disable_message_box;
@@ -138,7 +138,7 @@ void I_Error(const char *error, ...)
   }
 #endif
 
-  biz_error_callback((char *)&errmsg);
+  biz_error_callback(error);
   abort(); // Allow debugging
 }
 
