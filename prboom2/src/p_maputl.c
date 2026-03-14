21b357d35c43f19525f7689b9126ed178500f9c0
feos <feykomylce@gmail.com>
2026-03-14 18:34:54 +0300

    fire even if no intercepts were added, to catch tracelines

--- prboom2/src/p_maputl.c
+++ prboom2/src/p_maputl.c
@@ -443,8 +443,6 @@ void P_SetThingPosition(mobj_t *thing)
 // exit with false without checking anything else.
 //
 
-// 1/11/98 killough: Intercept limit removed
-intercept_t *intercepts, *intercept_p;
 dboolean PIT_AddLineIntercepts(line_t *ld);
 dboolean PIT_AddThingIntercepts(mobj_t *thing);
 extern void biz_intercept_callback(int block);
@@ -526,12 +524,11 @@ dboolean P_BlockLinesIterator(int x, int y, dboolean func(line_t*))
         continue;       // line has already been checked
 
       ld->validcount = validcount;
-      intercept_t *old_p = intercept_p;
 
       if (!func(ld))
         return false;
 
-      if (old_p != intercept_p && func == PIT_AddLineIntercepts)
+      if (func == PIT_AddLineIntercepts)
         biz_intercept_callback(y*bmapwidth+x);
     }
   return true;  // everything was checked
@@ -626,12 +623,10 @@ dboolean P_BlockThingsIterator(int x, int y, dboolean func(mobj_t*))
   if (!(x<0 || y<0 || x>=bmapwidth || y>=bmapheight))
     for (mobj = blocklinks[y*bmapwidth+x]; mobj; mobj = mobj->bnext)
     {
-      intercept_t *old_p = intercept_p;
-
       if (!func(mobj))
         return false;
-
-      if (old_p != intercept_p && func == PIT_AddThingIntercepts)
+        
+      if (func == PIT_AddThingIntercepts)
         biz_intercept_callback(y*bmapwidth+x);
     }
 
@@ -641,6 +636,9 @@ dboolean P_BlockThingsIterator(int x, int y, dboolean func(mobj_t*))
 //
 // INTERCEPT ROUTINES
 //
+
+// 1/11/98 killough: Intercept limit removed
+intercept_t *intercepts, *intercept_p;
 
 // Check for limit and double size if necessary -- killough
 void check_intercept(void)
